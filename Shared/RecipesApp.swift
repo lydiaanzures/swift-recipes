aab379f03a9eadd2ea8815ad44c40305b22f643e
lydiaanzures <lydiaanzures@gmail.com>
2022-01-28 03:01:54 -0500

    Initial Commit

---
+++ Shared/RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesApp.swift
+//  Shared
+//
+//  Created by Lydia Anzures on 1/28/22.
+//
+
+import SwiftUI
+
+@main
+struct RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
